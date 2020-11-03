fbdd67820c608aa5ea00b349c18781e3455fe790
Jordanw95 <65023872+Jordanw95@users.noreply.github.com>
2020-11-03 11:01:23 +0100

    Initial commit
    commit of full blog site.

---
+++ mysite/blog/urls.py
@@ -0,0 +1,22 @@
+from django.urls import path
+from blog import views
+
+
+
+
+urlpatterns = [
+    path('', views.PostListView.as_view(), name='post_list'),
+    path('about/', views.AboutView.as_view(), name='about'),
+    path('post/<int:pk>/', views.PostDetailView.as_view(), name='post_detail'),
+    path('post/new/', views.CreatePostView.as_view(), name = 'post_new'),
+    path('post/<int:pk>/edit/', views.PostUpdateView.as_view(), name='post_edit'),
+    path('post/<int:pk>/remove/', views.PostDeleteView.as_view(), name = 'post_remove'),
+    path('drafts/', views.DraftListView.as_view(), name = 'post_draft_list'),
+    path('post/<int:pk>/comment/', views.add_comment_to_post, name = 'add_comment_to_post'),
+    path('comments/<int:pk>/approve/', views.comment_approve, name = 'comment_approve'),
+    path('comments/<int:pk>/remove/', views.comment_remove, name = 'comment_remove'),
+    path('post/<int:pk>/publish/', views.post_publish, name='post_publish'),
+
+ ]
+
+ 
